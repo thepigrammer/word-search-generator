f0e26790b9056b95ba8ef8a339ddb19d3b9e5999
ThePigrammer <thepigrammer@gmail.com>
2025-08-13 14:50:03 -0500

    added comments for right() function; changed hash table insertions to append to start of list

--- wordsearch.c
+++ wordsearch.c
@@ -84,13 +84,13 @@ int main(void) {
         // Go through every column, based on length, for every row.
         for (unsigned int column = 0; column < length; column++) {
 
-            // Store index of hash table array that could hold a node for the current grid coordinate.
+            // Store index of hash table array that could hold a node for the current grid coordinates.
             unsigned int hashIndex = hash(column, row);
 
             // Begin at the first node of the linked list at hashIndex.
             node = letters[hashIndex];
 
-            // If there is no list at hashIndex, then there is no letter stored for current coordinate.
+            // If there is no list at hashIndex, then there is no letter stored for current coordinates.
             if (node == NULL) {
 
                 // Print a random letter.
@@ -100,7 +100,7 @@ int main(void) {
             // If there is only one node in the list at hashIndex...
             else if (node->next == NULL) {
 
-                // If the node is for the current coordinate...
+                // If the node is for the current coordinates...
                 if (node->column == column && node->row == row) {
 
                     // Print the letter held in the node.
@@ -111,7 +111,7 @@ int main(void) {
                     letters[hashIndex] == NULL;
                 }
 
-                // If the node is not for the current coordinate, then there is no letter stored here.
+                // If the node is not for the current coordinates, then there is no letter stored here.
                 else {
 
                     // Print a random letter.
@@ -122,7 +122,7 @@ int main(void) {
             }
             else {
 
-                // If the first node is for the current coordinate...
+                // If the first node is for the current coordinates...
                 if (node->column == column && node->row == row) {
 
                     // Print the letter in the node.
@@ -131,11 +131,11 @@ int main(void) {
                     // Set the start of hashIndex's list to the node after the old starting node.
                     letters[hashIndex] = node->next;
 
-                    // Free the current coordinate's node.
+                    // Free the current coordinates' node.
                     free(node);
                 }
 
-                // Else, there could be a node for the current coordinate after the first node.
+                // Else, there could be a node for the current coordinates after the first node.
                 else {
 
                     // Move through the list, checking the coordinates of the next node before moving to it.
@@ -143,14 +143,14 @@ int main(void) {
                         node = node->next;
                     }
 
-                    // If the current node is the end of the list, then there was no letter stored for current coordinate.
+                    // If the current node is the end of the list, then there was no letter stored for current coordinates.
                     if (node->next == NULL) {
 
                         // Print a random letter.
                         printf("%c ", 'A' + (rand() % 26));
                     }
 
-                    // Else, the next node must be for the current coordinate.
+                    // Else, the next node must be for the current coordinates.
                     else {
 
                         // Print the letter in the next node.
@@ -159,10 +159,10 @@ int main(void) {
                         // Store the next node for freeing.
                         freeing = node->next;
 
-                        // Set the current node's next field to point to the node after the current coordinate's node.
+                        // Set the current node's next field to point to the node after the current coordinates' node.
                         node->next = freeing->next;
 
-                        // Free the current coordinate's node.
+                        // Free the current coordinates' node.
                         free(freeing);
                     }
                 }
@@ -179,75 +179,107 @@ int main(void) {
     return 0;
 }
 
+// Hash function for hash table of letters.
 unsigned int hash(unsigned int column, unsigned int row) {
     return (column + row) % 50;
 }
 
+// Function for putting a word on the grid (AKA in the hash table) in the right-facing direction.
+// Returns true if the word was successfully placed or false if it was not.
 bool right(ListNode** letters, char* word, unsigned int wordLength, unsigned int length, unsigned int height) {
+
+    // If the word is longer than the length of the grid, then it cannot be placed horizontally.
     if (wordLength > length) {
         return false;
     }
     
+    // Declare row and column variables, setting the row to a random row on the grid.
     unsigned int row = rand() % height;
     unsigned int column;
 
+    // If the word is shorter than the length of each row...
     if (wordLength < length) {
+
+        // Choose a random column to start the word at, such that the rest will definitely fit placed to the right.
         column = rand() % (length - wordLength + 1);
     }
+
+    // Else, the word is the length of the grid, and must start at the first column to take up the entire row.
     else {
         column = 0;
     }
 
+    // Declare variables for traversing hash table and detecting if a spot on the grid is already taken.
     ListNode* node;
     bool spotTaken = false;
 
+    // Go through every letter in the word to detect if its potential grid spot is already taken.
     for (unsigned int i = 0; i < wordLength && spotTaken == false; i++) {
+
+        // Start at the first node of the hash table chain that the potential coordinates could already be in.
         node = letters[hash(column + i, row)];
-        while (node != NULL && spotTaken == false) {
-            if (!(node->column == column && node->row == row) || node->letter == word[i]) {
+
+        // Go through the linked list, searching for a node that already holds the coordinates.
+        while (node != NULL) {
+
+            // If current node does not hold the coordinates, move to the next node.
+            if (!(node->column == column && node->row == row)) {
                 node = node->next;
             }
+            
+            // Else if the node does hold the coordinates but the letter is the same, that works; move to next letter.
+            else if (node->letter == word[i]) {
+                break;
+            }
+
+            // Else, the potential spot is already taken by another letter.
             else {
                 spotTaken = true;
+                break;
             }
         }
     }
 
+    // If a grid spot was already taken, word could not be placed.
     if (spotTaken == true) {
         return false;
     }
 
+    // Word can be placed; make a variable to store the return value of the hash function to avoid unnecessary calls.
     unsigned int hashIndex;
+
+    // Go through each letter of the word and store it in the hash table, along with its coordinates.
     for (unsigned int i = 0; i < wordLength; i++) {
+
+        // Store the index of the hash table chain for the letter's coordinates.
         hashIndex = hash(column + i, row);
+
+        // Store the first node of the chain.
         node = letters[hashIndex];
+
+        // Make the new node, and set the array's element to point to it.
+        letters[hashIndex] = malloc(sizeof(ListNode));
+        if (letters[hashIndex] == NULL) {
+            printf("Error: malloc() returned NULL.");
+            return false;
+        }
+
+        // Set the values of the node with the coordinates and letter.
+        letters[hashIndex]->column = column + i;
+        letters[hashIndex]->row = row;
+        letters[hashIndex]->letter = word[i];
+
+        // If there was no existing linked list at the index, set next to NULL.
         if (node == NULL) {
-            letters[hashIndex] = malloc(sizeof(ListNode));
-            if (letters[hashIndex] == NULL) {
-                printf("Error: malloc() returned NULL.");
-                return false;
-            }
-            letters[hashIndex]->column = column + i;
-            letters[hashIndex]->row = row;
-            letters[hashIndex]->letter = word[i];
             letters[hashIndex]->next = NULL;
         }
+        
+        // Else, there was already a list at the index, so the new node points to the old starting node.
         else {
-            while (node->next != NULL) {
-                node = node->next;
-            }
-            node->next = malloc(sizeof(ListNode));
-            node = node->next;
-            if (node == NULL) {
-                printf("Error: malloc() returned NULL.");
-                return false;
-            }
-            node->column = column + i;
-            node->row = row;
-            node->letter = word[i];
-            node->next = NULL;
+            letters[hashIndex]->next = node;
         }
     }
 
+    // Return true to signal that the word was successfully placed in the hash table.
     return true;
 }
