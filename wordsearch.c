2246d1fa2e6c9e0f34be4844fa978a170b92f794
ThePigrammer <thepigrammer@gmail.com>
2025-08-16 17:55:44 -0500

    added left(); randomly chooses between which direction to try first after receiving word; removed creation of duplicate nodes for overlapping letters

--- wordsearch.c
+++ wordsearch.c
@@ -24,7 +24,9 @@ bool rightTopMiddle(ListNode** letters, char* word, unsigned int wordLength, uns
 bool rightBottomRight(ListNode** letters, char* word, unsigned int wordLength, unsigned int length, unsigned int height, unsigned int* row, unsigned int* column);
 bool rightBottomLeft(ListNode** letters, char* word, unsigned int wordLength, unsigned int length, unsigned int height, unsigned int* row, unsigned int* column);
 bool rightBottomMiddle(ListNode** letters, char* word, unsigned int wordLength, unsigned int length, unsigned int height, unsigned int* row, unsigned int* column);
-
+bool left(ListNode** letters, char* word, unsigned int wordLength, unsigned int length, unsigned int height);
+bool checkLeft(ListNode** letters, char* word, unsigned int wordLength, unsigned int* row, unsigned int* column);
+bool leftMiddleRight(ListNode** letters, char* word, unsigned int wordLength, unsigned int length, unsigned int height, unsigned int* row, unsigned int* column);
 
 int main(void) {
     
@@ -81,7 +83,7 @@ int main(void) {
                 wordSize *= 2;
             }
 
-            word[i] = letter;
+            word[i] = toupper(letter);
             wordLength++;
             letter = getchar();
         }
@@ -96,9 +98,44 @@ int main(void) {
             break;
         }
 
-        // Call right() to store the word in the hash table as individual letters with their grid coordinates.
-        // If right() returns false, indicate that the word could not be placed.
-        if (!right(letters, word, wordLength, length, height)) {
+        // Make an array with an element for each possible direction, with each element initialized to 0.
+        int functions[2] = {0};
+
+        // Store each direction's number in the array in random order.
+        int index;
+        for (int i = 1; i <= 2; i++) {
+            index = rand();
+
+            // While the potential element has already been used to store a direction's number, search for another.
+            while (functions[index % 2] != 0) {
+                index++;
+            }
+
+            functions[index % 2] = i;
+        }
+
+        // Create variables for storing direction function pointer and whether the call to it failed.
+        bool (*function)(ListNode** letters, char* word, unsigned int wordLength, unsigned int length, unsigned int height);
+        bool failed = true;
+
+        // Go through functions array, calling the function at element until word is placed or all directions tried.
+        for (int i = 0; failed && i < 2; i++) {
+            switch (functions[i]) {
+                case 1: 
+                    function = right;
+                    break;
+                case 2:
+                    function = left;
+            }
+            // Call function to store the word in the hash table as individual letters with their grid coordinates.
+            // If function returns false, indicate that the word could not be placed in that direction.
+            if (function(letters, word, wordLength, length, height)) {
+                failed = false;
+            }
+        }
+
+        // If failed is still true after all directions were checked, word was not placed.
+        if (failed) {
             printf("Word could not be placed on the grid.\n");
         }
 
@@ -219,6 +256,7 @@ unsigned int hash(unsigned int column, unsigned int row) {
     return (column + row) % 50;
 }
 
+
 // Function for putting a word on the grid (AKA in the hash table) in the right-facing direction.
 // Returns true if the word was successfully placed or false if it was not.
 bool right(ListNode** letters, char* word, unsigned int wordLength, unsigned int length, unsigned int height) {
@@ -231,7 +269,7 @@ bool right(ListNode** letters, char* word, unsigned int wordLength, unsigned int
     // Declare row and column variables.
     unsigned int row, column;
 
-    // Randomly define variable to hold function pointer of one of 6 searching algorithms.
+    // Randomly define variable to hold function pointer of one of 9 searching algorithms.
     bool (*function)(ListNode** letters, char* word, unsigned int wordLength, unsigned int length, unsigned int height, unsigned int* row, unsigned int* column);
     int random = rand() % 9;
     switch (random) {
@@ -276,6 +314,11 @@ bool right(ListNode** letters, char* word, unsigned int wordLength, unsigned int
     ListNode* node;
     for (unsigned int i = 0; i < wordLength; i++) {
 
+        // If letter already has a node, it was changed to lowercase by check function; continue to next letter.
+        if (word[i] == tolower(word[i])) {
+            continue;
+        }
+
         // Store the index of the hash table chain for the letter's coordinates.
         hashIndex = hash(column + i, row);
 
@@ -292,7 +335,7 @@ bool right(ListNode** letters, char* word, unsigned int wordLength, unsigned int
         // Set the values of the node with the coordinates and letter.
         letters[hashIndex]->column = column + i;
         letters[hashIndex]->row = row;
-        letters[hashIndex]->letter = toupper(word[i]);
+        letters[hashIndex]->letter = word[i];
 
         // If there was no existing linked list at the index, set next to NULL.
         if (node == NULL) {
@@ -331,12 +374,24 @@ bool checkRight(ListNode** letters, char* word, unsigned int wordLength, unsigne
             }
             
             // Else if the node does hold the coordinates but the letter is the same, that works; move to next letter.
-            else if (node->letter == toupper(word[i])) {
+            else if (node->letter == word[i]) {
+
+                // Mark that the letter does not need to have a node created in hash table later if word fits.
+                word[i] = tolower(word[i]);
+
                 break;
             }
 
             // Else, the potential spot is already taken by another letter.
             else {
+
+                // Reset the word's letters if any were changed to lowercase.
+                for (unsigned int j = 0; j < wordLength; j++) {
+                    if (word[j] != toupper(word[j])) {
+                        word[j] = toupper(word[j]);
+                    }
+                }
+
                 spotTaken = true;
                 break;
             }
@@ -990,6 +1045,236 @@ bool rightBottomMiddle(ListNode** letters, char* word, unsigned int wordLength, 
         *column = (length - 1) / 2;
         displace2 = 0;
         (*row)--;
+    }
+
+    // If no spot was found during the check, word cannot be placed anywhere, return false.
+    if (spotFound == false) {
+        return false;
+    }
+
+    return true;
+}
+
+
+// Function for putting a word on the grid in the left-facing (backwards) direction.
+bool left(ListNode** letters, char* word, unsigned int wordLength, unsigned int length, unsigned int height) {
+
+    // If the word is longer than the length of the grid, then it cannot be placed horizontally.
+    if (wordLength > length) {
+        return false;
+    }
+
+    // Declare row and column variables.
+    unsigned int row, column;
+
+    // Randomly define variable to hold function pointer of one of 9 searching algorithms.
+    bool (*function)(ListNode** letters, char* word, unsigned int wordLength, unsigned int length, unsigned int height, unsigned int* row, unsigned int* column);
+    int random = rand() % 1;
+    switch (random) {
+        case 0:
+            function = leftMiddleRight;
+            break;
+        case 1:
+            function = rightMiddleLeft;
+            break;
+        case 2:
+            function = rightMiddleMiddle;
+            break;
+        case 3:
+            function = rightTopRight;
+            break;
+        case 4:
+            function = rightTopLeft;
+            break;
+        case 5:
+            function = rightTopMiddle;
+            break;
+        case 6:
+            function = rightBottomRight;
+            break;
+        case 7:
+            function = rightBottomLeft;
+            break;
+        case 8:
+            function = rightBottomMiddle;
+            break;
+    }
+
+    // Check for spot and set row and column; if returns false, no spot found for word.
+    if (!function(letters, word, wordLength, length, height, &row, &column)) {
+        return false;
+    }
+
+    // Word can be placed; make a variable to store the return value of the hash function to avoid unnecessary calls.
+    unsigned int hashIndex;
+
+    // Go through each letter of the word and store it in the hash table, along with its coordinates.
+    ListNode* node;
+    for (unsigned int i = 0; i < wordLength; i++) {
+
+        // If letter already has a node, it was changed to lowercase by check function; continue to next letter.
+        if (word[i] == tolower(word[i])) {
+            continue;
+        }
+
+        // Store the index of the hash table chain for the letter's coordinates.
+        hashIndex = hash(column - i, row);
+
+        // Store the first node of the chain.
+        node = letters[hashIndex];
+
+        // Make the new node, and set the array's element to point to it.
+        letters[hashIndex] = malloc(sizeof(ListNode));
+        if (letters[hashIndex] == NULL) {
+            printf("Error: malloc() returned NULL.\n");
+            return false;
+        }
+
+        // Set the values of the node with the coordinates and letter.
+        letters[hashIndex]->column = column - i;
+        letters[hashIndex]->row = row;
+        letters[hashIndex]->letter = word[i];
+
+        // If there was no existing linked list at the index, set next to NULL.
+        if (node == NULL) {
+            letters[hashIndex]->next = NULL;
+        }
+        
+        // Else, there was already a list at the index, so the new node points to the old starting node.
+        else {
+            letters[hashIndex]->next = node;
+        }
+    }
+
+    // Return true to signal that the word was successfully placed in the hash table.
+    return true;
+}
+
+// Checks whether a word will fit facing left for the given coordinates.
+bool checkLeft(ListNode** letters, char* word, unsigned int wordLength, unsigned int* row, unsigned int* column) {
+
+    // Declare variable to signal whether the checked spot is taken and for node traversal.
+    bool spotTaken = false;
+    ListNode* node;
+
+    // Go through every letter in the word to detect if its potential grid spot is already taken.
+    for (unsigned int i = 0; i < wordLength && spotTaken == false; i++) {
+
+        // Start at the first node of the hash table chain that the potential coordinates could already be in.
+        node = letters[hash(*column - i, *row)];
+
+        // Go through the linked list, searching for a node that already holds the coordinates.
+        while (node != NULL) {
+
+            // If current node does not hold the coordinates, move to the next node.
+            if (!(node->column == *column - i && node->row == *row)) {
+                node = node->next;
+            }
+            
+            // Else if the node does hold the coordinates but the letter is the same, that works; move to next letter.
+            else if (node->letter == word[i]) {
+
+                // Mark that the letter does not need to have a node created in hash table later if word fits.
+                word[i] = tolower(word[i]);
+
+                break;
+            }
+
+            // Else, the potential spot is already taken by another letter.
+            else {
+
+                // Reset the word's letters if any were changed to lowercase.
+                for (unsigned int j = 0; j < wordLength; j++) {
+                    if (word[j] != toupper(word[j])) {
+                        word[j] = toupper(word[j]);
+                    }
+                }
+
+                spotTaken = true;
+                break;
+            }
+        }
+    }
+
+    // Return whether or not the spot is available.
+    return !spotTaken;
+}
+
+// Checks for a spot where word can be placed facing left, starting from middle row and rightmost column.
+bool leftMiddleRight(ListNode** letters, char* word, unsigned int wordLength, unsigned int length, unsigned int height, unsigned int* row, unsigned int* column) {
+
+    // Initialize row as the middle row of the grid.
+    *row = (height - 1) / 2;
+
+    // Set column to rightmost one.
+    *column = length - 1;
+
+    // Declare variables for holding displacement from middle row and the current row being checked.
+    int displace = 0;
+    unsigned int dispRow;
+
+    // Declare variable to signal whether a spot for the word has been found.
+    bool spotFound = false;
+
+    // Check every row, starting from middle row then alternating outward checks.
+    while (*row + displace < height) {
+
+        // If displace is negative, take it away from middle row to get current checking row.
+        if (displace < 0) {
+            dispRow = *row - (unsigned int)(displace * -1);
+        }
+
+        // Else, displace is positive and is added to middle row.
+        else {
+            dispRow = *row + (unsigned int)displace;
+        }
+        
+        // Check every column starting from current column to column 0.
+        while (true) {
+
+            // If function returns true, the spot is available, and passed row and column values are for that spot.
+            if (checkLeft(letters, word, wordLength, &dispRow, column)) {
+                spotFound = true;
+                break;
+            }
+
+            // If word will be too long going to left or column 0 was checked, there is no spot for word on the row.
+            if (wordLength - 1 == *column || *column == 0) {
+                break;
+            }
+
+            // Move column to the left by one.
+            (*column)--;
+        }
+
+        // If a spot for the word was found, set row to the row that the spot was found in and break from loop.
+        if (spotFound) {
+            *row = dispRow;
+            break;
+        }
+
+        // If the row is the middle, increment displace.
+        if (displace == 0) {
+            displace++;
+        }
+        
+        // Else if the displacement is negative, flip to positive and increment.
+        else if (displace < 0) {
+            displace = (displace * -1) + 1;
+        }
+
+        // Else if the displacement is positive and flipping to negative would be in bounds, flip to negative.
+        else if ((unsigned int)(displace) <= *row) {
+            displace *= -1;
+        }
+
+        // Else, the displacement would be out of bounds, all rows checked and loop complete.
+        else {
+            break;
+        }
+
+        // Reset column to the rightmost one since changing rows.
+        *column = length - 1;
     }
 
     // If no spot was found during the check, word cannot be placed anywhere, return false.
